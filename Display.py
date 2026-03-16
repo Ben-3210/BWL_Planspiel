b5e5aa29023fa486ff436b2d87a4bfb0c56dee5c
Ben <rollbahn.gib-5o@icloud.com>
2026-03-16 12:09:09 +0100

    streamlit

--- Display.py
+++ Display.py
@@ -1,1 +1,7 @@
-Testing
+import streamlit as st
+
+st.title("Mein Planspiel")
+st.write("Willkommen zur digitalen Version.")
+
+if st.button("Nächste Runde"):
+    st.write("Die Runde wurde gestartet.")
